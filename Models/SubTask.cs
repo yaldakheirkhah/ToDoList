1bfecfa8890de1a74d6989e4257f46dd9aaf8555
Yalda <yal.kheirkhah@gmail.com>
2023-03-27 15:39:03 +0330

    everything deleted

--- Models/SubTask.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ToDoList.Models;
-
-public abstract class SubTask
-{
-    public string? Description { get; set; }
-    public bool IsCompleted { get; set; }
-}
--- Models/Task.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ToDoList.Models;
-
-public abstract class Task
-{
-    public string? Name { get; set; }
-    public DateTime FromDate { get; set; }
-    public DateTime ToDate { get; set; }
-    public IList<SubTask>? SubTasks{ get; set; }
-}
