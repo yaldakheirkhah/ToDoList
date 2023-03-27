6205a49a247a23defd44566b1973b1b2ae83c4cd
Yalda <yal.kheirkhah@gmail.com>
2023-03-27 14:57:35 +0330

    Task class created

---
+++ Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoList.Models;
+
+public abstract class Task
+{
+    public string? Name { get; set; }
+    public DateTime FromDate { get; set; }
+    public DateTime ToDate { get; set; }
+    public IList<SubTask>? SubTasks{ get; set; }
+}
