86ad44caca339ab5502e4182fa621379c5b635f5
Yalda <yal.kheirkhah@gmail.com>
2023-03-28 10:46:16 +0330

    controller added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
